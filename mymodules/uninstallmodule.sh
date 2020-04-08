13f1f6757fab7ea627fbc350ef2e1b9123be42b8
hongbo.wang <whbo158@qq.com>
2020-04-08 17:14:06 +0800

    uninstallmodule.sh
    Signed-off-by: hongbo.wang <whbo158@qq.com>

---
+++ mymodules/uninstallmodule.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo sysrepoctl -i nxp-bridge-vlan-tc-flower.yang
+sudo sysrepoctl -i ieee802-types.yang
+sudo sysrepoctl -i ieee802-dot1q-types.yang
+sudo sysrepoctl -i ietf-interfaces@2014-05-08.yang
+sudo sysrepoctl -i ietf-inet-types@2013-07-15.yang
+sudo sysrepoctl -i iana-if-type@2017-01-19.yang
+sudo sysrepoctl -i ietf-ip@2014-06-16.yang -e ipv4-non-contiguous-netmasks
+sudo sysrepoctl -i ieee802-dot1q-bridge.yang
