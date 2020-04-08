0cdcdc2a8f7475cafe90c15ef913a758d4760b16
hongbo.wang <whbo158@qq.com>
2020-04-08 17:54:49 +0800

    files
    Signed-off-by: hongbo.wang <whbo158@qq.com>

---
+++ src/brtc_cfg.c
@@ -0,0 +1,469 @@
+/**
+ * @file brtc_cfg.c
+ * @author hongbo wang (hongbo.wang@nxp.com)
+ * @brief Application to configure bridge vlan based on sysrepo datastore.
+ *
+ * Copyright 2019-2020 NXP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "brtc_cfg.h"
+
+struct item_qdisc {
+	char action[MAX_PARA_LEN];
+	char block[MAX_PARA_LEN];
+	char ifname[MAX_PARA_LEN];
+};
+
+struct item_filter {
+	uint16_t vlan_id;
+	uint16_t priority;
+	uint16_t src_port;
+	uint16_t dst_port;
+
+	char src_ip[MAX_PARA_LEN];
+	char dst_ip[MAX_PARA_LEN];
+	char src_mac[MAX_PARA_LEN];
+	char dst_mac[MAX_PARA_LEN];
+	char action[MAX_PARA_LEN];
+	char protocol[MAX_PARA_LEN];
+	char parent[MAX_PARA_LEN];
+	char type[MAX_PARA_LEN];
+	char skip_type[MAX_PARA_LEN];
+	char ifname[MAX_PARA_LEN];
+	char action_spec[MAX_ACTION_LEN];
+};
+
+struct item_cfg {
+	bool valid;
+	uint32_t vid;
+	uint8_t sub_flag;
+	struct item_qdisc qdisc;
+	struct item_filter filter;
+};
+static struct item_cfg sitem_conf;
+
+static char stc_cmd[MAX_CMD_LEN];
+static char stc_subcmd[MAX_CMD_LEN];
+
+static int set_inet_vlan(char *ifname, int vid, bool addflag)
+{
+	int ret = 0;
+	int sockfd = 0;
+	struct vlan_ioctl_args ifr;
+	size_t max_len = sizeof(ifr.device1);
+
+	if (!ifname)
+		return -1;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		PRINT("create socket failed! ret:%d\n", sockfd);
+		return -2;
+	}
+
+	memset(&ifr, 0, sizeof(ifr));
+	ifr.u.VID = vid;
+
+	if (addflag) {
+		ifr.cmd = ADD_VLAN_CMD;
+		snprintf(ifr.device1, max_len, "%s", ifname);
+	} else {
+		ifr.cmd = DEL_VLAN_CMD;
+		snprintf(ifr.device1, max_len, "%s.%d", ifname, vid);
+	}
+
+	ret = ioctl(sockfd, SIOCSIFVLAN, &ifr);
+	close(sockfd);
+	if (ret < 0) {
+		PRINT("ioctl error! ret:%d, need root account!\n", ret);
+		PRINT("Note: this operation needs root permission!\n");
+		return -3;
+	}
+
+	return 0;
+}
+
+static int change_mac_format(char *pbuf)
+{
+	int i = 0;
+
+	if (!pbuf)
+		return -1;
+
+	for (i = 0; i < strlen(pbuf); i++) {
+		if (pbuf[i] == '-')
+			pbuf[i] = ':';
+	}
+
+	return 0;
+}
+
+static int parse_node(sr_session_ctx_t *session, sr_val_t *value,
+			struct item_cfg *conf)
+{
+	int rc = SR_ERR_OK;
+	sr_xpath_ctx_t xp_ctx = {0};
+	char *index = NULL;
+	uint8_t u8_val = 0;
+	uint32_t u32_val = 0;
+	uint64_t u64_val = 0;
+	char *strval = NULL;
+	char *nodename = NULL;
+	char err_msg[MSG_MAX_LEN] = {0};
+
+	if (!session || !value || !conf)
+		return rc;
+
+	sr_xpath_recover(&xp_ctx);
+	nodename = sr_xpath_node_name(value->xpath);
+	if (!nodename)
+		goto ret_tag;
+
+	strval = value->data.string_val;
+
+	if (!strcmp(nodename, "tc-flower-id")) {
+		memset(conf, 0, sizeof(struct item_cfg));
+	} else if (!strcmp(nodename, "qdisc")) {
+		conf->sub_flag = SUB_ITEM_QDISC;
+	} else if (!strcmp(nodename, "filter")) {
+		conf->sub_flag = SUB_ITEM_FILTER;
+	} else if (!strcmp(nodename, "action")) {
+		if (conf->sub_flag == SUB_ITEM_QDISC)
+			_PARA(conf->qdisc.action, MAX_PARA_LEN, strval);
+		else if (conf->sub_flag == SUB_ITEM_FILTER)
+			_PARA(conf->filter.action, MAX_PARA_LEN, strval);
+	} else if (!strcmp(nodename, "interface")) {
+		if (conf->sub_flag == SUB_ITEM_QDISC)
+			_PARA(conf->qdisc.ifname, MAX_PARA_LEN, strval);
+		else if (conf->sub_flag == SUB_ITEM_FILTER)
+			_PARA(conf->filter.ifname, MAX_PARA_LEN, strval);
+	} else if (!strcmp(nodename, "block")) {
+		if (conf->sub_flag == SUB_ITEM_QDISC)
+			_PARA(conf->qdisc.block, MAX_PARA_LEN, strval);
+	} else if (!strcmp(nodename, "protocol")) {
+		if (conf->sub_flag == SUB_ITEM_FILTER)
+			_PARA(conf->filter.protocol, MAX_PARA_LEN, strval);
+	} else if (!strcmp(nodename, "parent")) {
+		if (conf->sub_flag == SUB_ITEM_FILTER)
+			_PARA(conf->filter.parent, MAX_PARA_LEN, strval);
+	} else if (!strcmp(nodename, "filter_type")) {
+		if (conf->sub_flag == SUB_ITEM_FILTER)
+			_PARA(conf->filter.type, MAX_PARA_LEN, strval);
+	} else if (!strcmp(nodename, "skip_type")) {
+		if (conf->sub_flag == SUB_ITEM_FILTER)
+			_PARA(conf->filter.skip_type, MAX_PARA_LEN, strval);
+	} else if (!strcmp(nodename, "vlan_id")) {
+		if (conf->sub_flag == SUB_ITEM_FILTER)
+			conf->filter.vlan_id = value->data.uint16_val;
+	} else if (!strcmp(nodename, "priority")) {
+		if (conf->sub_flag == SUB_ITEM_FILTER)
+			conf->filter.priority = value->data.uint16_val;
+	} else if (!strcmp(nodename, "src_ip")) {
+		if (conf->sub_flag == SUB_ITEM_FILTER)
+			_PARA(conf->filter.src_ip, MAX_PARA_LEN, strval);
+	} else if (!strcmp(nodename, "dst_ip")) {
+		if (conf->sub_flag == SUB_ITEM_FILTER)
+			_PARA(conf->filter.dst_ip, MAX_PARA_LEN, strval);
+	} else if (!strcmp(nodename, "src_port")) {
+		if (conf->sub_flag == SUB_ITEM_FILTER)
+			conf->filter.src_port = value->data.uint16_val;
+	} else if (!strcmp(nodename, "dst_port")) {
+		if (conf->sub_flag == SUB_ITEM_FILTER)
+			conf->filter.dst_port = value->data.uint16_val;
+	} else if (!strcmp(nodename, "src_mac")) {
+		if (conf->sub_flag == SUB_ITEM_FILTER) {
+			_PARA(conf->filter.src_mac, MAX_PARA_LEN, strval);
+			change_mac_format(conf->filter.src_mac);
+		}
+	} else if (!strcmp(nodename, "dst_mac")) {
+		if (conf->sub_flag == SUB_ITEM_FILTER) {
+			_PARA(conf->filter.dst_mac, MAX_PARA_LEN, strval);
+			change_mac_format(conf->filter.dst_mac);
+		}
+	} else if (!strcmp(nodename, "action_spec")) {
+		if (conf->sub_flag == SUB_ITEM_FILTER) {
+			conf->valid = true;
+			_PARA(conf->filter.action_spec, MAX_ACTION_LEN, strval);
+		}
+	}
+
+ret_tag:
+	return rc;
+}
+
+static int parse_item(sr_session_ctx_t *session, char *path,
+			struct item_cfg *conf)
+{
+	size_t i;
+	size_t count;
+	int rc = SR_ERR_OK;
+	sr_val_t *values = NULL;
+	char err_msg[MSG_MAX_LEN] = {0};
+
+	rc = sr_get_items(session, path, 0, &values, &count);
+	if (rc == SR_ERR_NOT_FOUND) {
+		/*
+		 * If can't find any item, we should check whether this
+		 * container was deleted.
+		 */
+		if (is_del_oper(session, path)) {
+			printf("WARN: %s was deleted, disable %s",
+			       path, "this Instance.\n");
+			goto cleanup;
+		} else {
+			printf("WARN: %s sr_get_items: %s\n", __func__,
+			       sr_strerror(rc));
+			return SR_ERR_OK;
+		}
+	} else if (rc != SR_ERR_OK) {
+		snprintf(err_msg, MSG_MAX_LEN,
+			 "Get items from %s failed", path);
+		sr_set_error(session, err_msg, path);
+
+		printf("ERROR: %s sr_get_items: %s\n", __func__,
+		       sr_strerror(rc));
+		return rc;
+	}
+
+	for (i = 0; i < count; i++) {
+		if (values[i].type == SR_LIST_T
+		    || values[i].type == SR_CONTAINER_PRESENCE_T)
+			continue;
+
+		rc = parse_node(session, &values[i], conf);
+	}
+
+cleanup:
+	sr_free_values(values, count);
+
+	return rc;
+}
+
+static int parse_config(sr_session_ctx_t *session, const char *path)
+{
+	int rc = SR_ERR_OK;
+	sr_change_oper_t oper;
+	char *ifname = NULL;
+	sr_val_t *value = NULL;
+	sr_val_t *old_value = NULL;
+	sr_val_t *new_value = NULL;
+	sr_change_iter_t *it = NULL;
+	sr_xpath_ctx_t xp_ctx = {0};
+	char xpath[XPATH_MAX_LEN] = {0};
+	char err_msg[MSG_MAX_LEN] = {0};
+	char ifname_bak[MAX_VLAN_LEN] = {0};
+	struct item_cfg *conf = &sitem_conf;
+
+	memset(conf, 0, sizeof(struct item_cfg));
+
+	snprintf(xpath, XPATH_MAX_LEN, "%s//*", path);
+
+	rc = sr_get_changes_iter(session, xpath, &it);
+	if (rc != SR_ERR_OK) {
+		snprintf(err_msg, MSG_MAX_LEN,
+			 "Get changes from %s failed", xpath);
+		sr_set_error(session, err_msg, xpath);
+
+		printf("ERROR: %s sr_get_changes_iter: %s\n", __func__,
+		       sr_strerror(rc));
+		goto cleanup;
+	}
+
+	while (SR_ERR_OK == (rc = sr_get_change_next(session, it,
+					&oper, &old_value, &new_value))) {
+
+		value = new_value ? new_value : old_value;
+		if (!value)
+			continue;
+
+		ifname = sr_xpath_key_value(value->xpath, "bridge",
+					    "name", &xp_ctx);
+
+		sr_free_val(old_value);
+		sr_free_val(new_value);
+
+		if (!ifname)
+			continue;
+
+		if (!strcmp(ifname, ifname_bak))
+			continue;
+		snprintf(ifname_bak, MAX_VLAN_LEN, "%s", ifname);
+
+		rc = parse_item(session, xpath, conf);
+		if (rc != SR_ERR_OK)
+			break;
+	}
+
+	if (rc == SR_ERR_NOT_FOUND)
+		rc = SR_ERR_OK;
+
+cleanup:
+	return rc;
+}
+
+static int show_config(struct item_cfg *conf)
+{
+	if (!conf)
+		return -1;
+
+	printf("disc-action = %s\n", conf->qdisc.action);
+	printf("qdisc-interface = %s\n", conf->qdisc.ifname);
+	printf("disc-block = %s\n", conf->qdisc.block);
+	printf("filter-action = %s\n", conf->filter.action);
+	printf("filter-interface = %s\n", conf->filter.ifname);
+	printf("filter-protocol = %s\n", conf->filter.protocol);
+	printf("filter-parent = %s\n", conf->filter.parent);
+	printf("filter-type = %s\n", conf->filter.type);
+	printf("filter-skip_type = %s\n", conf->filter.skip_type);
+	printf("filter-vlan_id = %d\n", conf->filter.vlan_id);
+	printf("filter-priority = %d\n", conf->filter.priority);
+	printf("filter-src_ip = %s\n", conf->filter.src_ip);
+	printf("filter-dst_ip = %s\n", conf->filter.dst_ip);
+	printf("filter-src_port = %d\n", conf->filter.src_port);
+	printf("filter-dst_port = %d\n", conf->filter.dst_port);
+	printf("filter-src_mac = %s\n", conf->filter.src_mac);
+	printf("filter-dst_mac = %s\n", conf->filter.dst_mac);
+	printf("filter-action_spec = %s\n", conf->filter.action_spec);
+
+	return 0;
+}
+
+static int set_config(sr_session_ctx_t *session, bool abort)
+{
+	int rc = SR_ERR_OK;
+	struct item_cfg *conf = &sitem_conf;
+
+	if (abort) {
+		memset(conf, 0, sizeof(struct item_cfg));
+		return rc;
+	}
+
+	if (!conf->valid)
+		return rc;
+
+//	show_config(conf);
+
+	if ((strlen(conf->qdisc.action) == 0)
+			|| (strlen(conf->qdisc.ifname) == 0))
+		return rc;
+
+	if ((strlen(conf->filter.action) == 0)
+			|| (strlen(conf->filter.ifname) == 0))
+		return rc;
+
+	snprintf(stc_cmd, MAX_CMD_LEN, "tc qdisc %s dev %s %s\n",
+		conf->qdisc.action, conf->qdisc.ifname, conf->qdisc.block);
+	printf("qdisc: %s\n", stc_cmd);
+	system(stc_cmd);
+
+	snprintf(stc_cmd, MAX_CMD_LEN, "tc filter %s dev %s ",
+		conf->filter.action, conf->filter.ifname);
+
+	if (strlen(conf->filter.parent) > 0) {
+		snprintf(stc_subcmd, MAX_CMD_LEN, "parent %s ",
+				conf->filter.parent);
+		strncat(stc_cmd, stc_subcmd, MAX_CMD_LEN - 1 - strlen(stc_cmd));
+	}
+	if (strlen(conf->filter.protocol) > 0) {
+		snprintf(stc_subcmd, MAX_CMD_LEN, "protocol %s ",
+				conf->filter.protocol);
+		strncat(stc_cmd, stc_subcmd, MAX_CMD_LEN - 1 - strlen(stc_cmd));
+	}
+	if (strlen(conf->filter.type) > 0) {
+		snprintf(stc_subcmd, MAX_CMD_LEN, "%s ", conf->filter.type);
+		strncat(stc_cmd, stc_subcmd, MAX_CMD_LEN - 1 - strlen(stc_cmd));
+	}
+	if (strlen(conf->filter.skip_type) > 0) {
+		snprintf(stc_subcmd, MAX_CMD_LEN, "%s ",
+				conf->filter.skip_type);
+		strncat(stc_cmd, stc_subcmd, MAX_CMD_LEN - 1 - strlen(stc_cmd));
+	}
+	if (conf->filter.vlan_id > 0) {
+		snprintf(stc_subcmd, MAX_CMD_LEN, "vlan_id %d ",
+				conf->filter.vlan_id);
+		strncat(stc_cmd, stc_subcmd, MAX_CMD_LEN - 1 - strlen(stc_cmd));
+	}
+	if (conf->filter.priority > 0) {
+		snprintf(stc_subcmd, MAX_CMD_LEN, "vlan_prio %d ",
+				conf->filter.priority);
+		strncat(stc_cmd, stc_subcmd, MAX_CMD_LEN - 1 - strlen(stc_cmd));
+	}
+	if (strlen(conf->filter.src_ip) > 0) {
+		snprintf(stc_subcmd, MAX_CMD_LEN, "src_ip %s ",
+				conf->filter.src_ip);
+		strncat(stc_cmd, stc_subcmd, MAX_CMD_LEN - 1 - strlen(stc_cmd));
+	}
+	if (strlen(conf->filter.dst_ip) > 0) {
+		snprintf(stc_subcmd, MAX_CMD_LEN, "dst_ip %s ",
+				conf->filter.dst_ip);
+		strncat(stc_cmd, stc_subcmd, MAX_CMD_LEN - 1 - strlen(stc_cmd));
+	}
+	if (conf->filter.src_port > 0) {
+		snprintf(stc_subcmd, MAX_CMD_LEN, "src_port %d ",
+				conf->filter.src_port);
+		strncat(stc_cmd, stc_subcmd, MAX_CMD_LEN - 1 - strlen(stc_cmd));
+	}
+	if (conf->filter.dst_port > 0) {
+		snprintf(stc_subcmd, MAX_CMD_LEN, "dst_port %d ",
+				conf->filter.dst_port);
+		strncat(stc_cmd, stc_subcmd, MAX_CMD_LEN - 1 - strlen(stc_cmd));
+	}
+	if (strlen(conf->filter.src_mac) > 0) {
+		snprintf(stc_subcmd, MAX_CMD_LEN, "src_mac %s ",
+				conf->filter.src_mac);
+		strncat(stc_cmd, stc_subcmd, MAX_CMD_LEN - 1 - strlen(stc_cmd));
+	}
+	if (strlen(conf->filter.dst_mac) > 0) {
+		snprintf(stc_subcmd, MAX_CMD_LEN, "dst_mac %s ",
+				conf->filter.dst_mac);
+		strncat(stc_cmd, stc_subcmd, MAX_CMD_LEN - 1 - strlen(stc_cmd));
+	}
+	if (strlen(conf->filter.action_spec) > 0) {
+		snprintf(stc_subcmd, MAX_CMD_LEN, "action %s ",
+				conf->filter.action_spec);
+		strncat(stc_cmd, stc_subcmd, MAX_CMD_LEN - 1 - strlen(stc_cmd));
+	}
+	printf("filter: %s\n", stc_cmd);
+	system(stc_cmd);
+
+	return rc;
+}
+
+int brtc_subtree_change_cb(sr_session_ctx_t *session, const char *module_name,
+	const char *path, sr_event_t event, uint32_t id, void *private_ctx)
+{
+	int rc = SR_ERR_OK;
+	char xpath[XPATH_MAX_LEN] = {0};
+
+	snprintf(xpath, XPATH_MAX_LEN, "%s", path);
+
+	switch (event) {
+	case SR_EV_CHANGE:
+		rc = parse_config(session, xpath);
+		break;
+	case SR_EV_ENABLED:
+		break;
+	case SR_EV_DONE:
+		rc = set_config(session, false);
+		break;
+	case SR_EV_ABORT:
+		rc = set_config(session, true);
+		break;
+	default:
+		break;
+	}
+
+	return rc;
+}
---
+++ src/vlan_cfg.c
@@ -0,0 +1,262 @@
+/**
+ * @file vlan_cfg.c
+ * @author hongbo wang (hongbo.wang@nxp.com)
+ * @brief Application to configure VLAN based on sysrepo datastore.
+ *
+ * Copyright 2019-2020 NXP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "vlan_cfg.h"
+
+struct item_cfg {
+	bool valid;
+	bool vidflag;
+	uint32_t vid;
+	char ifname[IF_NAME_MAX_LEN];
+};
+
+static struct item_cfg sitem_conf;
+
+static int set_inet_vlan(char *ifname, int vid, bool addflag)
+{
+	int ret = 0;
+	int sockfd = 0;
+	struct vlan_ioctl_args ifr;
+	size_t max_len = sizeof(ifr.device1);
+
+	if (!ifname)
+		return -1;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		PRINT("create socket failed! ret:%d\n", sockfd);
+		return -2;
+	}
+
+	memset(&ifr, 0, sizeof(ifr));
+	ifr.u.VID = vid;
+
+	if (addflag) {
+		ifr.cmd = ADD_VLAN_CMD;
+		snprintf(ifr.device1, max_len, "%s", ifname);
+	} else {
+		ifr.cmd = DEL_VLAN_CMD;
+		snprintf(ifr.device1, max_len, "%s.%d", ifname, vid);
+	}
+
+	ret = ioctl(sockfd, SIOCSIFVLAN, &ifr);
+	close(sockfd);
+	if (ret < 0) {
+		PRINT("ioctl error! ret:%d, need root account!\n", ret);
+		PRINT("Note: this operation needs root permission!\n");
+		return -3;
+	}
+
+	return 0;
+}
+
+static int parse_node(sr_session_ctx_t *session, sr_val_t *value,
+			struct item_cfg *conf)
+{
+	int rc = SR_ERR_OK;
+	sr_xpath_ctx_t xp_ctx = {0};
+	char *index = NULL;
+	uint8_t u8_val = 0;
+	uint32_t u32_val = 0;
+	uint64_t u64_val = 0;
+	char *strval = NULL;
+	char *nodename = NULL;
+	char err_msg[MSG_MAX_LEN] = {0};
+
+	if (!session || !value || !conf)
+		return rc;
+
+	sr_xpath_recover(&xp_ctx);
+	nodename = sr_xpath_node_name(value->xpath);
+	if (!nodename)
+		goto ret_tag;
+
+	if (!strcmp(nodename, "vid")) {
+		if (value->data.uint32_val > 0) {
+			conf->vid = value->data.uint32_val;
+			conf->vidflag = true;
+		}
+	} else if (!strcmp(nodename, "name")) {
+		if (conf->vidflag) {
+			snprintf(conf->ifname, IF_NAME_MAX_LEN, "%s",
+						value->data.string_val);
+			conf->valid = true;
+		}
+	}
+
+ret_tag:
+	return rc;
+}
+
+static int parse_item(sr_session_ctx_t *session, char *path,
+			struct item_cfg *conf)
+{
+	size_t i;
+	size_t count;
+	int rc = SR_ERR_OK;
+	sr_val_t *values = NULL;
+	char err_msg[MSG_MAX_LEN] = {0};
+
+	rc = sr_get_items(session, path, 0, &values, &count);
+	if (rc == SR_ERR_NOT_FOUND) {
+		/*
+		 * If can't find any item, we should check whether this
+		 * container was deleted.
+		 */
+		if (is_del_oper(session, path)) {
+			printf("WARN: %s was deleted, disable %s",
+			       path, "this Instance.\n");
+			goto cleanup;
+		} else {
+			printf("WARN: %s sr_get_items: %s\n", __func__,
+			       sr_strerror(rc));
+			return SR_ERR_OK;
+		}
+	} else if (rc != SR_ERR_OK) {
+		snprintf(err_msg, MSG_MAX_LEN,
+			 "Get items from %s failed", path);
+		sr_set_error(session, err_msg, path);
+
+		printf("ERROR: %s sr_get_items: %s\n", __func__,
+		       sr_strerror(rc));
+		return rc;
+	}
+
+	for (i = 0; i < count; i++) {
+		if (values[i].type == SR_LIST_T
+		    || values[i].type == SR_CONTAINER_PRESENCE_T)
+			continue;
+
+		rc = parse_node(session, &values[i], conf);
+	}
+
+cleanup:
+	sr_free_values(values, count);
+
+	return rc;
+}
+
+static int parse_config(sr_session_ctx_t *session, const char *path)
+{
+	int rc = SR_ERR_OK;
+	sr_change_oper_t oper;
+	char *vid = NULL;
+	sr_val_t *value = NULL;
+	sr_val_t *old_value = NULL;
+	sr_val_t *new_value = NULL;
+	sr_change_iter_t *it = NULL;
+	sr_xpath_ctx_t xp_ctx = {0};
+	char xpath[XPATH_MAX_LEN] = {0};
+	char err_msg[MSG_MAX_LEN] = {0};
+	char vid_bak[MAX_VLAN_LEN] = {0};
+	struct item_cfg *conf = &sitem_conf;
+
+	memset(conf, 0, sizeof(struct item_cfg));
+
+	snprintf(xpath, XPATH_MAX_LEN, "%s//*", path);
+
+	rc = sr_get_changes_iter(session, xpath, &it);
+	if (rc != SR_ERR_OK) {
+		snprintf(err_msg, MSG_MAX_LEN,
+			 "Get changes from %s failed", xpath);
+		sr_set_error(session, err_msg, xpath);
+
+		printf("ERROR: %s sr_get_changes_iter: %s\n", __func__,
+		       sr_strerror(rc));
+		goto cleanup;
+	}
+
+	while (SR_ERR_OK == (rc = sr_get_change_next(session, it,
+					&oper, &old_value, &new_value))) {
+
+		value = new_value ? new_value : old_value;
+		if (!value)
+			continue;
+
+		vid = sr_xpath_key_value(value->xpath, "vlan",
+					    "vid", &xp_ctx);
+
+		sr_free_val(old_value);
+		sr_free_val(new_value);
+
+		if (!vid)
+			continue;
+
+		if (!strcmp(vid, vid_bak))
+			continue;
+		snprintf(vid_bak, MAX_VLAN_LEN, "%s", vid);
+
+		rc = parse_item(session, xpath, conf);
+		if (rc != SR_ERR_OK)
+			break;
+	}
+
+cleanup:
+	if (rc == SR_ERR_NOT_FOUND)
+		rc = SR_ERR_OK;
+
+	return rc;
+}
+
+static int set_config(sr_session_ctx_t *session, bool abort)
+{
+	int rc = SR_ERR_OK;
+	struct item_cfg *conf = &sitem_conf;
+
+	if (abort) {
+		memset(conf, 0, sizeof(struct item_cfg));
+		return rc;
+	}
+
+	if (!conf->valid)
+		return rc;
+
+	set_inet_vlan(conf->ifname, conf->vid, true);
+	PRINT("set_inet_vlan ifname:%s vid:%d\n", conf->ifname, conf->vid);
+
+	return rc;
+}
+
+int vlan_subtree_change_cb(sr_session_ctx_t *session, const char *module_name,
+	const char *path, sr_event_t event, uint32_t id, void *private_ctx)
+{
+	int rc = SR_ERR_OK;
+	char xpath[XPATH_MAX_LEN] = {0};
+
+	snprintf(xpath, XPATH_MAX_LEN, "%s", path);
+
+	switch (event) {
+	case SR_EV_CHANGE:
+		rc = parse_config(session, xpath);
+		break;
+	case SR_EV_ENABLED:
+		break;
+	case SR_EV_DONE:
+		rc = set_config(session, false);
+		break;
+	case SR_EV_ABORT:
+		rc = set_config(session, true);
+		break;
+	default:
+		break;
+	}
+
+	return rc;
+}
