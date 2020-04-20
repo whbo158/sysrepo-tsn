cf86c852de0873ab08a08c7a9f0976e9957e3a4b
hongbo.wang <hongbo.wang@nxp.com>
2020-04-20 13:24:38 +0800

    del sudo
    Signed-off-by: hongbo.wang <hongbo.wang@nxp.com>

--- mymodules/installmodule.sh
+++ mymodules/installmodule.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo sysrepoctl -i ieee802-types.yang
-sudo sysrepoctl -i ieee802-dot1q-types.yang
-sudo sysrepoctl -i ietf-interfaces@2014-05-08.yang
-sudo sysrepoctl -i ietf-inet-types@2013-07-15.yang
-sudo sysrepoctl -i iana-if-type@2017-01-19.yang
-sudo sysrepoctl -i ietf-ip@2014-06-16.yang -e ipv4-non-contiguous-netmasks
-sudo sysrepoctl -i ieee802-dot1q-bridge.yang
-sudo sysrepoctl -i ieee802-dot1q-sched.yang -e scheduled-traffic
-sudo sysrepoctl -i nxp-bridge-vlan-tc-flower.yang
+sysrepoctl -i ieee802-types.yang
+sysrepoctl -i ieee802-dot1q-types.yang
+sysrepoctl -i ietf-interfaces@2014-05-08.yang
+sysrepoctl -i ietf-inet-types@2013-07-15.yang
+sysrepoctl -i iana-if-type@2017-01-19.yang
+sysrepoctl -i ietf-ip@2014-06-16.yang -e ipv4-non-contiguous-netmasks
+sysrepoctl -i ieee802-dot1q-bridge.yang
+sysrepoctl -i ieee802-dot1q-sched.yang -e scheduled-traffic
+sysrepoctl -i nxp-bridge-vlan-tc-flower.yang
--- mymodules/uninstallmodule.sh
+++ mymodules/uninstallmodule.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo sysrepoctl -u nxp-bridge-vlan-tc-flower.yang
-sudo sysrepoctl -u ieee802-dot1q-sched.yang
-sudo sysrepoctl -u ieee802-types.yang
-sudo sysrepoctl -u ieee802-dot1q-types.yang
-sudo sysrepoctl -u ietf-interfaces@2014-05-08.yang
-sudo sysrepoctl -u ietf-inet-types@2013-07-15.yang
-sudo sysrepoctl -u iana-if-type@2017-01-19.yang
-sudo sysrepoctl -u ietf-ip@2014-06-16.yang -e ipv4-non-contiguous-netmasks
-sudo sysrepoctl -u ieee802-dot1q-bridge.yang
+sysrepoctl -u nxp-bridge-vlan-tc-flower.yang
+sysrepoctl -u ieee802-dot1q-sched.yang
+sysrepoctl -u ieee802-types.yang
+sysrepoctl -u ieee802-dot1q-types.yang
+sysrepoctl -u ietf-interfaces@2014-05-08.yang
+sysrepoctl -u ietf-inet-types@2013-07-15.yang
+sysrepoctl -u iana-if-type@2017-01-19.yang
+sysrepoctl -u ietf-ip@2014-06-16.yang -e ipv4-non-contiguous-netmasks
+sysrepoctl -u ieee802-dot1q-bridge.yang
