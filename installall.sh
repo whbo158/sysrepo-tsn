ac5373a7e2505b27d2152c44a2567d64d970a183
hongbo.wang <hongbo.wang@nxp.com>
2020-04-20 13:20:32 +0800

    del sudo
    Signed-off-by: hongbo.wang <hongbo.wang@nxp.com>

--- installall.sh
+++ installall.sh
@@ -6,34 +6,34 @@ mkdir libyang/build
 cd libyang/build/
 cmake ..
 make
-sudo make install
+make install
 cd -
 
 mkdir sysrepo/build
 cd sysrepo/build/
 cmake ..
 make
-sudo make install
+make install
 cd -
 
 mkdir libnetconf2/build
 cd libnetconf2/build/
 cmake ..
 make
-sudo make install
+make install
 cd -
 
 mkdir Netopeer2/server/build
 cd Netopeer2/server/build/
 cmake ..
 make
-sudo make install
+make install
 cd -
 
 mkdir Netopeer2/cli/build
 cd Netopeer2/cli/build/
 cmake ..
 make
-sudo make install
+make install
 cd -
 
--- uninstallall.sh
+++ uninstallall.sh
@@ -3,26 +3,26 @@
 cd /home/whb/netopeer
 
 cd Netopeer2/server/build/
-sudo make uninstall
+make uninstall
 rm -fr *
 cd -
 
 cd Netopeer2/cli/build/
-sudo make uninstall
+make uninstall
 rm -fr *
 cd -
 
 cd libnetconf2/build/
-sudo make uninstall
+make uninstall
 rm -fr *
 cd -
 
 cd sysrepo/build/
-sudo make uninstall
+make uninstall
 rm -fr *
 cd -
 
 cd libyang/build/
-sudo make uninstall
+make uninstall
 rm -fr *
 cd -
