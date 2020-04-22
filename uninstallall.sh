733908f796a011fbbfcba21949416d9f05cad330
hongbo.wang <whbo158@qq.com>
2020-04-22 13:53:50 +0800

    new
    Signed-off-by: hongbo.wang <whbo158@qq.com>

--- uninstallall.sh
+++ uninstallall.sh
@@ -3,26 +3,26 @@
 cd /home/whb/netopeer
 
 cd Netopeer2/server/build/
-make uninstall
+sudo make uninstall
 rm -fr *
 cd -
 
 cd Netopeer2/cli/build/
-make uninstall
+sudo make uninstall
 rm -fr *
 cd -
 
 cd libnetconf2/build/
-make uninstall
+sudo make uninstall
 rm -fr *
 cd -
 
 cd sysrepo/build/
-make uninstall
+sudo make uninstall
 rm -fr *
 cd -
 
 cd libyang/build/
-make uninstall
+sudo make uninstall
 rm -fr *
 cd -
