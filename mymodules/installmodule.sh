b886325183a36ee1e8df37e9569684bf8a8dbbe1
hongbo.wang <whbo158@qq.com>
2020-04-28 13:41:11 +0800

    change install script
    Signed-off-by: hongbo.wang <whbo158@qq.com>

--- mymodules/installmodule.sh
+++ mymodules/installmodule.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sysrepoctl -i ieee802-types.yang
-sysrepoctl -i ieee802-dot1q-types.yang
-sysrepoctl -i ietf-interfaces@2014-05-08.yang
-sysrepoctl -i ietf-inet-types@2013-07-15.yang
-sysrepoctl -i iana-if-type@2017-01-19.yang
-sysrepoctl -i ietf-ip@2014-06-16.yang -e ipv4-non-contiguous-netmasks
-sysrepoctl -i ieee802-dot1q-bridge.yang
-sysrepoctl -i ieee802-dot1q-sched.yang -e scheduled-traffic
-sysrepoctl -i nxp-bridge-vlan-tc-flower.yang
+sudo sysrepoctl -i --yang=ieee802-types.yang
+sudo sysrepoctl -i --yang=ieee802-dot1q-types.yang
+sudo sysrepoctl -i --yang=ietf-interfaces@2014-05-08.yang
+sudo sysrepoctl -i --yang=ietf-inet-types@2013-07-15.yang
+sudo sysrepoctl -i --yang=iana-if-type@2017-01-19.yang
+sudo sysrepoctl -i --yang=ietf-ip@2014-06-16.yang
+sudo sysrepoctl -i --feature-enable=ipv4-non-contiguous-netmasks --module=ietf-ip
+sudo sysrepoctl -i --yang=ieee802-dot1q-bridge.yang
+sudo sysrepoctl -i --yang=ieee802-dot1q-sched.yang
+sudo sysrepoctl -i --feature-enable=scheduled-traffic --module=ieee802-dot1q-sched
+sudo sysrepoctl -i --yang=nxp-bridge-vlan-tc-flower.yang
