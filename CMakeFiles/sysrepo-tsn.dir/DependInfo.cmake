a1171f3836cebe5db5d629d87a74be46d99e2d76
hongbo.wang <hongbo.wang@nxp.com>
2020-08-20 13:57:05 +0800

    sync from openil
    Signed-off-by: hongbo.wang <hongbo.wang@nxp.com>

---
+++ CMakeFiles/sysrepo-tsn.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/src/brtc_cfg.c" "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/CMakeFiles/sysrepo-tsn.dir/src/brtc_cfg.c.o"
+  "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/src/cb_streamid.c" "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/CMakeFiles/sysrepo-tsn.dir/src/cb_streamid.c.o"
+  "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/src/common.c" "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/CMakeFiles/sysrepo-tsn.dir/src/common.c.o"
+  "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/src/file_mon.c" "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/CMakeFiles/sysrepo-tsn.dir/src/file_mon.c.o"
+  "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/src/ip_cfg.c" "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/CMakeFiles/sysrepo-tsn.dir/src/ip_cfg.c.o"
+  "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/src/mac_cfg.c" "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/CMakeFiles/sysrepo-tsn.dir/src/mac_cfg.c.o"
+  "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/src/main.c" "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/CMakeFiles/sysrepo-tsn.dir/src/main.c.o"
+  "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/src/qbu.c" "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/CMakeFiles/sysrepo-tsn.dir/src/qbu.c.o"
+  "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/src/qbv.c" "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/CMakeFiles/sysrepo-tsn.dir/src/qbv.c.o"
+  "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/src/qci.c" "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/CMakeFiles/sysrepo-tsn.dir/src/qci.c.o"
+  "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/src/qci_fm.c" "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/CMakeFiles/sysrepo-tsn.dir/src/qci_fm.c.o"
+  "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/src/qci_sf.c" "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/CMakeFiles/sysrepo-tsn.dir/src/qci_sf.c.o"
+  "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/src/qci_sg.c" "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/CMakeFiles/sysrepo-tsn.dir/src/qci_sg.c.o"
+  "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/src/vlan_cfg.c" "/mnt/vda3/test_netconf/openil_1028/output/build/qoriq-sysrepo-tsn-v0.2/CMakeFiles/sysrepo-tsn.dir/src/vlan_cfg.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "inc"
+  "/mnt/vda3/test_netconf/openil_1028/output/host/aarch64-buildroot-linux-gnu/sysroot/usr/include/libnl3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/sysrepo-tsn.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sysrepo-tsn.dir/src/main.c.o"
+  "CMakeFiles/sysrepo-tsn.dir/src/common.c.o"
+  "CMakeFiles/sysrepo-tsn.dir/src/qbv.c.o"
+  "CMakeFiles/sysrepo-tsn.dir/src/qbu.c.o"
+  "CMakeFiles/sysrepo-tsn.dir/src/cb_streamid.c.o"
+  "CMakeFiles/sysrepo-tsn.dir/src/qci.c.o"
+  "CMakeFiles/sysrepo-tsn.dir/src/qci_sf.c.o"
+  "CMakeFiles/sysrepo-tsn.dir/src/qci_sg.c.o"
+  "CMakeFiles/sysrepo-tsn.dir/src/qci_fm.c.o"
+  "CMakeFiles/sysrepo-tsn.dir/src/brtc_cfg.c.o"
+  "CMakeFiles/sysrepo-tsn.dir/src/ip_cfg.c.o"
+  "CMakeFiles/sysrepo-tsn.dir/src/mac_cfg.c.o"
+  "CMakeFiles/sysrepo-tsn.dir/src/vlan_cfg.c.o"
+  "CMakeFiles/sysrepo-tsn.dir/src/file_mon.c.o"
+  "sysrepo-tsn.pdb"
+  "sysrepo-tsn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sysrepo-tsn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
